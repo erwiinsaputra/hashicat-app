dcd15bedb752a57a7065f39e4e99634834071819
erwiinsaputra <im.erwinsaputra@gmail.com>
2023-09-27 04:34:08 +0000

    Updated HTML

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -12,6 +12,7 @@ cat << EOM > /var/www/html/index.html
 
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
+  <center><h1>Welcome to $(PREFIX)'S app, Replice this text with your own</h1></center>
   <center><h2>Meow World!</h2></center>
   Welcome to ${PREFIX}'s app. Replace this text with your own.
   <!-- END -->
